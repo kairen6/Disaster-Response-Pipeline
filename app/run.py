8bd38ff597e5251671bbdf77416a995baf0ea2f7
Kai <kai.ren@foxmail.com>
2020-02-07 00:13:13 -0500

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,17 +1,23 @@
+import re
 import json
 import plotly
 import pandas as pd
-
+import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
+from nltk.corpus import stopwords
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
+import collections
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
-
+stop_words = stopwords.words("english")
+lemmatizer = WordNetLemmatizer()
 app = Flask(__name__)
 
 def tokenize(text):
@@ -25,6 +31,21 @@ def tokenize(text):
 
     return clean_tokens
 
+def tokenizepp(text):
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # lemmatize andremove stop words
+    tokens = [lemmatizer.lemmatize(word) for word in tokens if word not in stop_words]
+
+    return tokens
+
+
+
+
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
 df = pd.read_sql_table('table_one', engine)
@@ -37,14 +58,30 @@ model = joblib.load("../models/classifier.pkl")
 @app.route('/')
 @app.route('/index')
 def index():
-    cd
+    
+    #top words
+
+    X = df["message"].values  
+    l1=[tokenizepp(x) for x in X]   
+    concat_list = [j for i in l1 for j in i]
+    count=collections.Counter(concat_list)
+    top_words=count.most_common()[:10]
+    wordlist=[]
+    numberlist=[]
+
+    for i in range(0,10):
+        wordlist.append(top_words[i][0])
+        numberlist.append(top_words[i][1])
+
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    cats = df[df.columns[5:]]
+    cats_counts = cats.mean()*cats.shape[0]
+    cats_names = list(cats_counts.index)
     
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
+
     graphs = [
         {
             'data': [
@@ -63,8 +100,47 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        {
+            'data': [
+                Bar(
+                    x=wordlist,
+                    y=numberlist
+                )
+            ],
+
+            'layout': {
+                'title': 'Top Words',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
+        },
+        {
+            'data': [
+                Bar(
+                    x=cats_names,
+                    y=cats_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution of Message categories',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Category"
+                }
+            }
         }
+        
     ]
+    
+    
     
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
@@ -93,8 +169,7 @@ def go():
 
 
 def main():
-    
-    app.run(host='0.0.0.0', port=3001,debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 
 if __name__ == '__main__':
